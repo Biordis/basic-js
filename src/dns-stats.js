a00eb7938c26400d2596f9b136db9500ff23c14f
Biordis <filato.v@mail.ru>
2023-12-12 12:07:02 +0300

    feat: add functions for dns

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const result = {};
+  for (let i = 0; i < domains.length; i++) {
+    const domain = domains[i].split('.').reverse();
+    let str = '';
+    for (let j = 0; j < domain.length; j++) {
+      str += '.' + domain[j];
+      if (!result[str]) {
+        result[str] = 1;
+      } else {
+        result[str]++;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
